27571e4db98f78b365069f195a1e09d0449ad483
pireson <Peter@Peters-MacBook.local>
2014-06-17 02:43:10 +0100

    Navigation Added

---
+++ PHP Source/nav..php
@@ -0,0 +1,29 @@
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button> 
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="main_login.php">Login</a></li>
+        <li><a href="round_display.php">Scoring</a></li>
+        <li><a href="logout.php">Logout</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="main_login.php">Login</a></li>
+            <li><a href="round_display.php">Scoring</a></li>
+            <li><a href="logout.php">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
